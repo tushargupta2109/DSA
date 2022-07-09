be6e164b67544e20076cac2642a898f2c9936412
Tushar Gupta <82646611+tushargupta2109@users.noreply.github.com>
2022-07-09 23:23:38 +0530

    Time: 6 ms (33.65%), Space: 9.2 MB (29.20%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        if(n==0){
-            return;
+        int i=0,j;
+        for(j=m;j<(n+m);j++){
+            nums1[j] = nums2[i++];
         }
         
-        int j;
-        for(int i=0;i<m;i++){
-            if(nums1[i] > nums2[0]){
-                swap(nums1[i], nums2[0]);
-            }
+        int gap = ceil(float(n+m)/2);
+        while(gap>0){
             
-            int first = nums2[0];
-            for(j=1;j<n && nums2[j]<first;j++){
-                nums2[j-1] = nums2[j];
+            i=0, j=gap;
+            while(j<(n+m)){
+                if(nums1[i]>nums1[j]){
+                    swap(nums1[i], nums1[j]);
+                }
+                i++, j++;
             }
-            nums2[j-1] = first;
-        }
-        
-        j =0;
-        for(int i=m;i<(n+m);i++){
-            nums1[i] = nums2[j++];
+            if(gap == 1){
+                gap = 0;
+            }
+            else{
+                gap = ceil(float(gap)/2);
+            }
         }
     }
 };
