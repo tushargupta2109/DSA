5b4ac146932c1f3479d8de4fd0398abf0ff2308c
Tushar Gupta <82646611+tushargupta2109@users.noreply.github.com>
2022-07-18 00:43:19 +0530

    Time: 21 ms (50.65%), Space: 15.8 MB (81.68%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        int first=-1,second=-1;
-        int a=0, b=0;
         int n = nums.size();
+        int first= -1, second = -1;
+        int a=0, b=0;
         
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==first){
+        for(int i=0;i<n;i++){
+            if(nums[i] == first){
                 a++;
             }
-            else if(nums[i]==second)
-            {
+            else if(nums[i] == second){
                 b++;
             }
             else if(a==0){
                 first = nums[i];
                 a=1;
             }
-            else if(b==0)
-            {
+            else if(b==0){
                 second = nums[i];
-                b=1;
+                b =1;
             }
             else{
                 a--;
@@ -30,26 +27,22 @@ public:
             }
         }
         
-        int c1=0,c2=0;
+        int c1 =0, c2 = 0;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]==first)
-            {
+            if(nums[i] == first){
                 c1++;
             }
-            else if(nums[i]==second)
-            {
+            else if(nums[i] == second){
                 c2++;
             }
         }
         
         vector<int> ans;
-        if(c1>n/3)
-        {
+        if(c1 > n/3){
             ans.push_back(first);
         }
-        if(c2>n/3)
-        {
+        if(c2 > n/3){
             ans.push_back(second);
         }
         
