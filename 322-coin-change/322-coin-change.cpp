1449c65612c3885ed0f54568c585a559046893be
Tushar Gupta <82646611+tushargupta2109@users.noreply.github.com>
2022-08-12 01:53:16 +0530

    Time: 223 ms (19.71%), Space: 45 MB (29.64%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,31 +3,29 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        // vector<int> dp(amount+1, -1);
-        
-        // sort(coins.begin(), coins.end());
-        for(int i=0;i<=amount;i++){
-            dp[0][i] = INT_MAX - 1; 
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0; 
-        }
+        vector<int> prev(amount+1, INT_MAX - 1);
+        prev[0] = 0;
         
         for(int i=1;i<=n;i++){
+            
+            vector<int> curr(amount+1, -1);
+            curr[0] = 0;
+            
             for(int j=1;j<=amount;j++){
                 
                 int taken = INT_MAX - 1;
                 if(coins[i-1]<= j){
-                    taken = 1 + dp[i][j-coins[i-1]];
+                    taken = 1 + curr[j-coins[i-1]];
                 }
-                int not_taken = dp[i-1][j];
+                int not_taken = prev[j];
                 
-                dp[i][j] = min(not_taken, taken);
+                curr[j] = min(not_taken, taken);
             }
+            
+            prev = curr;
         }
         
-        int ans = dp[n][amount];
+        int ans = prev[amount];
         if(ans == INT_MAX-1){
             return -1;
         }
