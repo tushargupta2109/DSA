e284c31113855cb44111d46fa0d11e7424751ca6
Tushar Gupta <82646611+tushargupta2109@users.noreply.github.com>
2022-08-14 01:05:00 +0530

    Time: 153 ms (28.33%), Space: 10.3 MB (64.96%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int cal(vector<vector<int>> &dp,vector<int> &cuts, int st, int en, int left, int right)
+    {
+        int mn=INT_MAX;
+        if(st>en)
+            return 0;
+        if(dp[st][en]!=-1)
+            return dp[st][en];
+        
+        for(int i=st;i<=en;i++)
+        {
+            mn=min(mn, right-left+cal(dp,cuts, i+1, en, cuts[i], right)+cal(dp,cuts, st, i-1, left, cuts[i]));
+        }
+        
+        return dp[st][en] = mn;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(), cuts.end());
+        vector<vector<int>> dp(cuts.size(), vector<int> (cuts.size(), -1));
+        return cal(dp,cuts,0,cuts.size()-1,0,n);
+    }
+};
